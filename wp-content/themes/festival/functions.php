b09ddf78a9f47ef332577f67384d20366203a27a
Pierre Lamotte <lamotte@icm6.etu>
2013-01-30 10:21:10 +0100

    Suppression fchiers inutiles ou obsoletes 2

--- wp-content/themes/festival/functions.php
+++ wp-content/themes/festival/functions.php
@@ -1,31 +1,30 @@
-<script src="js/responsiveslides.min.js"></script>
-
 <?php
-function load_slideshow_script() {
-	if (is_home()) {
-		wp_enqueue_script('slideshow', get_template_directory_uri() . '/js/slideshow.js', array(‘jquery’);
-		wp_enqueue_script('init_slideshow', get_template_directory_uri(). '/js/init_slideshow.js', array('slideshow');
-	}
-}
-add_action('get_header', 'load_slideshow_script');
-?>
 
-<?php 
-function recupImageUne(){
-	if ( $post->post_type == 'data-design' && $post->post_status == 'publish' ) {
-		$attachments = get_posts( array(
-			'post_type' => 'attachment',
-			'posts_per_page' => -1,
-			'post_parent' => $post->ID,
-			'exclude' => get_post_thumbnail_id()
-		) );
-	}
-	if ( $attachments ) {
-		foreach ( $attachments as $attachment ) {
-			$class = "post-attachment mime-" . sanitize_title($attachment->post_mime_type );
-			$thumbimg = wp_get_attachment_link( $attachment->ID, 'thumbnail-size', true );
-			echo '<li class="' . $class . ' data-design-thumbnail">' .$thumbimg . '</li>';
-		}
+function load_slideshow_script() {
+	if (is_home() || is_front_page()) {
+		wp_enqueue_script('slideshow', get_stylesheet_directory_uri() . '/js/slideshow.js', array('jquery'));
+		wp_enqueue_script('init_slideshow', get_stylesheet_directory_uri() . '/js/init_slideshow.js', array('slideshow'));
 	}
 }
+
+add_action('get_header', 'load_slideshow_script');
+
+function getSlideItem(){
+	$attachments = get_posts( array(
+		'post_type' => 'attachment',
+		'posts_per_page' => -1,
+		'post_parent' => $post->ID,
+		'exclude' => get_post_thumbnail_id()
+		) );
+
+	if ( $attachments ) {
+		$output =array();
+		foreach ( $attachments as $attachment ) {
+			$thumb = wp_get_attachment_link( $attachment->ID, 'large', true );
+			$output[] = '<li>' . $thumb. '</li>';
+		}
+		echo implode("\n", $output);
+	}
+}
+
 ?>
